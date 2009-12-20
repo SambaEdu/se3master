bb91b1480b574fbc30ad5ae6397bf10ed33561b1
crob <stephane.boireau@ac-rouen.fr>
2009-12-20 14:03:56 +0000

    Ajout de la possibilité de réinitialiser les mots de passe d'après un CSV.

--- usr/share/se3/sbin/se3_reinit_mdp.sh
+++ usr/share/se3/sbin/se3_reinit_mdp.sh
@@ -18,12 +18,26 @@ then
 		echo "             sh $0 Eleves"
 		echo "        Vous pouvez aussi mettre 'alea' en \$2 pour mettre des mots de passe"
 		echo "        aleatoires."
+		echo "        Dernière alternative: Fournir en parametre \$1 la chaine:"
+		echo "             sh $0 csv=CHEMIN/FICHIER.csv"
+		echo "        au format:"
+		echo "             LOGIN;MDP;"
+		echo "        pour imposer les mots de passe d'apres le fichier fourni."
+		echo "        Remarque: Le ';' de fin est destine a eviter des blagues avec les fins"
+		echo "                  de lignes do$/unix."
 		exit
 fi
 
-BASEDN=$(cat /etc/ldap/ldap.conf | grep "^BASE" | tr "\t" " " | sed -e "s/ \{2,\}/ /g" | cut -d" " -f2)
-ROOTDN=$(cat /etc/ldap/slapd.conf | grep "^rootdn" | tr "\t" " " | cut -d'"' -f2)
-PASSDN=$(cat /etc/ldap.secret)
+. /usr/share/se3/sbin/variables_admin_ldap.sh lib > /dev/null
+
+# Si le variables_admin_ldap.sh n'est pas assez recent
+if [ -z "$BASEDN" -o -z "$ROOTDN" -o -z "$PASSDN" ]; then
+	# On utilise les parametres locaux... en esperant que le ldap est bien local
+	echo "On utilise les parametres locaux... en esperant que le ldap est bien local"
+	BASEDN=$(cat /etc/ldap/ldap.conf | grep "^BASE" | tr "\t" " " | sed -e "s/ \{2,\}/ /g" | cut -d" " -f2)
+	ROOTDN=$(cat /etc/ldap/slapd.conf | grep "^rootdn" | tr "\t" " " | cut -d'"' -f2)
+	PASSDN=$(cat /etc/ldap.secret)
+fi
 
 GEN_MDP() {
 	nbcarmdp=8
@@ -56,6 +70,8 @@ fi
 
 groupe=$1
 
+fichcsv=""
+
 if [ "$2" = "alea" ]; then
 	alea=y
 	dest=/home/admin/Bureau/changement_mdp_${1}_$(date +%Y%m%d%H%M%S).csv
@@ -63,56 +79,76 @@ if [ "$2" = "alea" ]; then
 	chown admin ${dest}
 else
 	alea=n
+	if [ "${1:0:4}" = "csv=" -a -e "${1:4}" ]; then
+		fichcsv=${1:4}
+	fi
 fi
 
-ldapsearch -xLLL cn=$groupe | grep memberUid | while read A
-do
-	uid=$(echo "$A" | cut -d" " -f2)
-	if [ "$alea" = "n" ]; then
-		# On fait une reinitialisation a la date de naissance le mot de passe:
-		date=$(ldapsearch -xLLL uid=$uid | grep "^gecos:" | cut -d"," -f2)
-		if smbclient -L 127.0.0.1 -U $uid%$date > /dev/null 2>&1; then
-			echo -e "$uid: \tLa date de naissance est le mot de passe."
-		else
-			tmp_test=$(echo "$date" | sed -e "s/[0-9]//g")
-			if [ -z "${tmp_test}" -a ! -z "$date" ]; then
-				echo -e "$uid: \tReinitialisation du mot de passe a $date:\c"
-				/usr/share/se3/sbin/userChangePwd.pl $uid $date
-				if [ "$?" = "0" ]; then
-					echo "OK"
+if [ -n "$fichcsv" ]; then
+	while read ligne
+	do
+		uid=$(echo "$ligne"|cut -d";" -f1)
+		pass=$(echo "$ligne"|cut -d";" -f2)
+		if [ -n "$uid" -a -n "$pass" ]; then
+			t=$(ldapsearch -xLLL -b ou=People,$BASEDN uid=$uid)
+			if [ -z "$t" ]; then
+				echo "Le login $uid n'existe pas dans la branche People de l'annuaire."
+			else
+				echo -e "$uid: \tModificatiation du mot de passe en $pass"
+				/usr/share/se3/sbin/userChangePwd.pl $uid $pass
+			fi
+		fi
+	done < $fichcsv
+else
+	ldapsearch -xLLL cn=$groupe | grep memberUid | while read A
+	do
+		uid=$(echo "$A" | cut -d" " -f2)
+		if [ "$alea" = "n" ]; then
+			# On fait une reinitialisation a la date de naissance le mot de passe:
+			date=$(ldapsearch -xLLL uid=$uid | grep "^gecos:" | cut -d"," -f2)
+			if smbclient -L 127.0.0.1 -U $uid%$date > /dev/null 2>&1; then
+				echo -e "$uid: \tLa date de naissance est le mot de passe."
+			else
+				tmp_test=$(echo "$date" | sed -e "s/[0-9]//g")
+				if [ -z "${tmp_test}" -a ! -z "$date" ]; then
+					echo -e "$uid: \tReinitialisation du mot de passe a $date:\c"
+					/usr/share/se3/sbin/userChangePwd.pl $uid $date
+					if [ "$?" = "0" ]; then
+						echo "OK"
+					else
+						echo "ERREUR"
+					fi
 				else
-					echo "ERREUR"
+					echo "ERREUR (mot de passe non identifie)"
+				fi
+			fi
+		else
+			# On met un mot de passe aleatoire
+			mdp=$(GEN_MDP)
+	
+			mail=$(ldapsearch -xLLL uid=$uid mail | grep "^mail:" | sed -e "s/^mail: //")
+			nom=$(ldapsearch -xLLL uid=$uid sn | grep "^sn:" | sed -e "s/^sn: //")
+			prenom=$(ldapsearch -xLLL uid=$uid givenName | grep "^givenName:" | sed -e "s/^givenName: //")
+	
+			classe=""
+			if [ "$groupe" = "Eleves" ]; then
+				classe=$(ldapsearch -xLLL "(&(memberUid=$uid)(cn=Classe_*))" cn | grep "^cn:" | sed -e "s/^cn: //")
+			fi
+	
+			if [ -n "$mdp" ]; then
+	
+				/usr/share/se3/sbin/userChangePwd.pl $uid $mdp
+				if [ "$?" = "0" ]; then
+					echo "$groupe;$nom;$prenom;$mail;$uid;$mdp;$classe" | tee -a $dest
+				else
+					echo "$groupe;$nom;$prenom;$mail;$uid;ECHEC changement MDP;$classe" | tee -a $dest
 				fi
 			else
-				echo "ERREUR (mot de passe non identifie)"
+				echo "$groupe;$nom;$prenom;$mail;$uid;ECHEC generation MDP???;$classe" | tee -a $dest
 			fi
 		fi
-	else
-		# On met un mot de passe aleatoire
-		mdp=$(GEN_MDP)
-
-		mail=$(ldapsearch -xLLL uid=$uid mail | grep "^mail:" | sed -e "s/^mail: //")
-		nom=$(ldapsearch -xLLL uid=$uid sn | grep "^sn:" | sed -e "s/^sn: //")
-		prenom=$(ldapsearch -xLLL uid=$uid givenName | grep "^givenName:" | sed -e "s/^givenName: //")
-
-		classe=""
-		if [ "$groupe" = "Eleves" ]; then
-			classe=$(ldapsearch -xLLL "(&(memberUid=$uid)(cn=Classe_*))" cn | grep "^cn:" | sed -e "s/^cn: //")
-		fi
-
-		if [ -n "$mdp" ]; then
-
-			/usr/share/se3/sbin/userChangePwd.pl $uid $mdp
-			if [ "$?" = "0" ]; then
-				echo "$groupe;$nom;$prenom;$mail;$uid;$mdp;$classe" | tee -a $dest
-			else
-				echo "$groupe;$nom;$prenom;$mail;$uid;ECHEC changement MDP;$classe" | tee -a $dest
-			fi
-		else
-			echo "$groupe;$nom;$prenom;$mail;$uid;ECHEC generation MDP???;$classe" | tee -a $dest
-		fi
-	fi
-done
+	done
+fi
 
 if [ "$alea" = "y" ]; then
 	echo "Un fichier CSV a ete genere en"
