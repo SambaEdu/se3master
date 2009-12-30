d9256fc7c688e0a6ad91cecfc1251c893691215e
keyser <keyser@033478fe-9347-4027-94cf-8a8f83edcaf9>
2009-12-30 21:02:20 +0000

    fusion avec la version 1.50

--- var/cache/se3_install/maj/maj120.sh
+++ var/cache/se3_install/maj/maj120.sh
@@ -5,7 +5,7 @@
 # path fichier de logs
 LOG_DIR="/var/log/se3"
 HISTORIQUE_MAJ="$LOG_DIR/historique_maj"
-REPORT_FILE=$LOG_DIR/log_maj110
+REPORT_FILE=$LOG_DIR/log_maj120
 
 #mode debug on si =1
 [ -e /root/debug ] && DEBUG="1"
@@ -15,6 +15,9 @@ LADATE=$(date +%d-%m-%Y)
 
 ### recup pass root mysql
 . /root/.my.cnf 2>/dev/null
+. /usr/share/se3/includes/config.inc.sh -lm
+. /usr/share/se3/includes/functions.inc.sh
+
 
 MAIL_REPORT()
 {
@@ -22,62 +25,18 @@ MAIL_REPORT()
 if [ ! -z "$MAIL_ADMIN" ]; then
 	REPORT=$(cat $REPORT_FILE)
 	#On envoie un mail à l'admin
-	echo "$REPORT"  | mail -s "[SE3] Résultat de la Mise a jour 110" $MAIL_ADMIN
+	echo "$REPORT"  | mail -s "[SE3] Résultat de la Mise a jour 120" $MAIL_ADMIN
 fi
 }
 
 
-POURSUIVRE()
-{
-	[ -n "$1" ] && echo "$1"
-	REPONSE=""
-	while [ "$REPONSE" != "o" -a "$REPONSE" != "O" -a "$REPONSE" != "n" ]
-	do
-		#echo -e "$COLTXT"
-		echo -e "${COLTXT}Peut-on poursuivre ? (${COLCHOIX}O/n${COLTXT}) $COLSAISIE\c"
-		read REPONSE
-		if [ -z "$REPONSE" ]; then
-			REPONSE="o"
-		fi
-	done
-	echo -e "$COLTXT"
-	if [ "$REPONSE" != "o" -a "$REPONSE" != "O" ]; then
-		echo "Abandon!"
-		exit 1
-	fi
-}
-
-# Recuperation de variables LDAP
-BASEDN=`echo "SELECT value FROM params WHERE name='ldap_base_dn'" | mysql -h $dbhost $dbname -u $dbuser -p$dbpass -N`
-if [ -z "$BASEDN" ]; then
-	echo "Impossible d'accéder au paramètre BASEDN"
-	exit 1
-fi
-ADMINRDN=`echo "SELECT value FROM params WHERE name='adminRdn'" | mysql -h $dbhost $dbname -u $dbuser -p$dbpass -N`
-if [ -z "$ADMINRDN" ]; then
-	echo "Impossible d'accéder au paramètre ADMINRDN"
-	exit 1
-fi
-ADMINPW=`echo "SELECT value FROM params WHERE name='adminPw'" | mysql -h $dbhost $dbname -u $dbuser -p$dbpass -N`
-if [ -z "$ADMINPW" ]; then
-	echo "Impossible d'accéder au paramètre ADMINPW"
-	exit 1
-fi
-PEOPLERDN=`echo "SELECT value FROM params WHERE name='peopleRdn'" | mysql -h $dbhost $dbname -u $dbuser -p$dbpass -N`
-if [ -z "$PEOPLERDN" ]; then
-	echo "Impossible d'accéder au paramètre PEOPLERDN"
-	exit 1
-fi
-GROUPSRDN=`echo "SELECT value FROM params WHERE name='groupsRdn'" | mysql -h $dbhost $dbname -u $dbuser -p$dbpass -N`
-if [ -z "$GROUPSRDN" ]; then
-	echo "Impossible d'accéder au paramètre GROUPSRDN"
-	exit 1
-fi
-RIGHTSRDN=`echo "SELECT value FROM params WHERE name='rightsRdn'" | mysql -h $dbhost $dbname -u $dbuser -p$dbpass -N`
-if [ -z "$RIGHTSRDN" ]; then
-	echo "Impossible d'accéder au paramètre RIGHTSRDN"
-	exit 1
-fi
+LDAPIP="$ldap_server"
+BASEDN="$ldap_base_dn"
+ADMINRDN="$adminRdn"
+ADMINPW="$adminPw"
+PEOPLERDN="$peopleRdn"
+GROUPSRDN="$groupsRdn"
+RIGHTSRDN="$rightsRdn"
 
 PEOPLER=`echo $PEOPLERDN |cut -d = -f 2`
 RIGHTSR=`echo $RIGHTSRDN |cut -d = -f 2`
@@ -86,42 +45,242 @@ GROUPSR=`echo $GROUPSRDN |cut -d = -f 2`
 # Correction nss pour ignorer root et openldap
 [ -z "$(grep "nss_initgroups_ignoreusers" /etc/libnss-ldap.conf)" ] && echo "nss_initgroups_ignoreusers root,openldap,plugdev,disk,kmem,tape,audio,daemon,lp,rdma,fuse,video,dialout,floppy,cdrom,tty" >> /etc/libnss-ldap.conf
 
-# Correction de droits
-echo "Remise en place des droits - exécution de permse3"
-/usr/share/se3/sbin/permse3
+# echo  "Ajout des groupes Samba3 obligatoires dans LDAP"
+DOMAINSID=`net getlocalsid | cut -d: -f2 | sed -e "s/ //g"`
+cat ldif/Samba.ldif | sed -e "s/#BASEDN#/$BASEDN/g" | sed -e "s/#DOMAINSID#/$DOMAINSID/g" | sed -e "s/#GROUPS#/$GROUPSR/g" | sed -e "s/#PEOPLE#/$PEOPLER/g" > /root/Samba_maj120.ldif 
+# ldapadd -x -c -D "$ADMINRDN,$BASEDN" -w $ADMINPW -f /root/Samba_maj120.ldif
 
-# DOMAINSID=`net getlocalsid | cut -d: -f2 | sed -e "s/ //g"`
-# ajout dbo pour mappage des groupes de bases
-# net groupmap list | grep "\bAdmins\b" ||net groupmap add sid=$DOMAINSID-512 ntgroup=Admins unixgroup=admins type=domain comment="Administrateurs du domaine"
-# # net groupmap list | grep "\bEleves\b" || net groupmap add sid=$DOMAINSID-516 ntgroup=Eleves unixgroup=Eleves type=domain comment="Eleves du domaine"
-# # net groupmap list | grep "\bProfs\b" || net groupmap add sid=$DOMAINSID-515 ntgroup=Profs unixgroup=Profs type=domain comment="Profs du domaine"
-# net groupmap list | grep "\bmachines\b" || net groupmap add sid=$DOMAINSID-553 ntgroup="Domain Computers" unixgroup=machines type=domain comment="Machines du domaine"
+#Change le SambaPrimaryGroupe
+echo "Modification du SambaPrimaryGroupe en arriere plan dans 2mn"
+AT_SCRIPT=/root/modif_SambaPrimaryGroupe.sh
+echo "#!/bin/bash
+ldapsearch -x -b $PEOPLERDN,$BASEDN '(objectclass=*)' uid | grep -v People | grep -v \# | grep uid: | while read A
+do
 
-# copie nouvelle version sudoers
-cp conf/sudoers /etc/sudoers
-chmod 0440 /etc/sudoers
+ID=`echo \$A | cut -d : -f 2 | cut -b 2-`
+        if [ "\$ID" != "" ]
+        then
+ldapmodify -x -v -D "$ADMINRDN,$BASEDN" -w "$ADMINPW" > /dev/null 2>&1 <<EOF
+dn: uid=\$ID,$PEOPLERDN,$BASEDN
+changetype: modify
+replace: sambaPrimaryGroupSID
+sambaPrimaryGroupSID: $DOMAINSID-513
+EOF
 
-# Mise en place conf samba
-/usr/share/se3/sbin/update-smbconf.sh
+	fi
 
-# Mise en place scripts de connexion (defaults)
-echo "Mise en place scripts de connexions... (veuillez patienter)"
-/usr/share/se3/sbin/update-share.sh -d
+done
+" >$AT_SCRIPT
+chmod 700 $AT_SCRIPT
+at now +2 minutes -f $AT_SCRIPT >/dev/null
 
-# Creation compte adminse3 dans annuaire
-/usr/share/se3/sbin/create_adminse3.sh
+echo  "Ajout des groupes Samba3 obligatoires dans LDAP"
+# mappage de nobody, root, etc...
+ldapsearch -xLLL "(&(sambaprimarygroupsid=S-1-5-32-546)(uid=nobody)(sambasid=$DOMAINSID-501))" cn | grep nobody > /dev/null 2>&1 || \
+echo "dn: uid=nobody,$PEOPLERDN,$BASEDN
+cn: nobody
+uid: nobody
+description: samba guest domain account
+gecos: samba guest domain account
+loginShell: /bin/false
+sambaAcctFlags: [NU         ]
+sambaPwdMustChange: 2147483647
+sambaPwdCanChange: 0
+sambaKickoffTime: 2147483647
+sambaLogoffTime: 2147483647
+sambaLogonTime: 0
+sambaPwdLastSet: 0
+homeDirectory: /dev/null
+objectClass: inetOrgPerson
+objectClass: sambaSamAccount
+objectClass: posixAccount
+objectClass: shadowAccount
+objectClass: organizationalPerson
+objectClass: top
+objectClass: person
+sn: nobody
+gidNumber: 65534
+sambaPrimaryGroupSID: S-1-5-32-546
+uidNumber: 65534
+sambaSID: $DOMAINSID-501" | ldapadd -x -h $LDAPIP -D $ADMINRDN,$BASEDN -w $ADMINPW
 
-# Mise a jour cron
-cp /var/cache/se3_install/conf/se3-crontab /etc/cron.d/se3
-/etc/init.d/cron restart >/dev/null 2>&1
+ldapsearch -xLLL "(&(sambasid=S-1-5-32-546)(objectclass=posixgroup))" cn | grep nogroup > /dev/null 2>&1 || \
+echo "dn: cn=nogroup,$GROUPSRDN,$BASEDN
+sambaGroupType: 2
+description: le groupe fantome
+objectClass: posixGroup
+objectClass: top
+objectClass: sambaGroupMapping
+gidNumber: 65534
+sambaSID: S-1-5-32-546
+displayName: nogroup
+cn: nogroup
+memberUid: nobody" | ldapadd -x -h $LDAPIP -D $ADMINRDN,$BASEDN -w $ADMINPW 
 
-echo "Mise a jour 110:
+ldapsearch -xLLL "cn=root -b $GROUPSRDN,$BASEDN" cn | grep root > /dev/null 2>&1 || \
+echo "dn: cn=root,$GROUPSRDN,$BASEDN
+objectClass: posixGroup
+objectClass: sambaGroupMapping
+gidNumber: 0
+cn: root
+description: groupe root samba
+memberUid: root
+sambaGroupType: 4
+sambaSID: $DOMAINSID-0
+displayName: Roots" | ldapadd -x -h $LDAPIP -D $ADMINRDN,$BASEDN -w $ADMINPW
+
+ldapsearch -xLLL "(&(sambasid=$DOMAINSID-514)(objectclass=posixgroup))" cn | grep invites > /dev/null 2>&1 || \
+echo "dn: cn=invites,$GROUPSRDN,$BASEDN
+objectClass: posixGroup
+objectClass: sambaGroupMapping
+gidNumber: 999
+cn: invites
+description: Invites du domaine
+memberUid: nobody
+sambaGroupType: 4
+sambaSID: $DOMAINSID-514
+displayName: Invites" | ldapadd -x -h $LDAPIP -D $ADMINRDN,$BASEDN -w $ADMINPW 
+
+
+# mappage des groupes particuliers
+echo "Mapping des groupes particuliers"
+
+# groupe lcs-users mappé vers "utilisateurs du domaine -513"
+net groupmap list | grep "lcs-users"  > /dev/null 2>&1 || net groupmap add ntgroup="Utilisateurs du domaine" rid="513" unixgroup="lcs-users" type="domain"
+
+# groupe machines mappé vers -515
+net groupmap list | grep "machines"  > /dev/null 2>&1 || net groupmap add ntgroup="machines" rid="515" unixgroup="machines" type="domain"
+
+# groupe admins mappé vers -512
+net groupmap list | grep "admins"  > /dev/null 2>&1 || net groupmap  add ntgroup="Admins" rid="512" unixgroup="admins" type="domain"
+
+# groupe profs / eleves rid auto
+net groupmap list | grep "Profs"  > /dev/null 2>&1 || net groupmap add ntgroup="Profs" unixgroup="Profs" type="domain" comment="Profs du domaine"
+net groupmap list | grep "Eleves"  > /dev/null 2>&1 || net groupmap add ntgroup="Eleves" unixgroup="Eleves" type="domain" comment="Eleves du domaine"
+
+# echo "on mappe tous les groupes"
+# echo "voulez vous supprimer les groupes cours ?
+# si vous ne les utilisez pas, le gain de performance peut etre important. 
+# ATTENTION 
+# pour les serveurs en Sarge, un utilisateur ne peut appartenir a plus de 32 groupes
+# Il est donc conseille de supprimer les cours.
+# Pour Etch cette limite n'existe pas
+# o/N"
+# read rep
+# if [ "$rep" == "o" ]; then 
+# 	ldapsearch -xLLL -h $LDAPIP -b $GROUPSRDN,$BASEDN "(cn=Cours_*)" dn | sed "s/dn: //g" | \
+# 		 ldapdelete -x -h $LDAPIP -D $ADMINRDN,$BASEDN -w $ADMINPW
+# 	echo " Groupes cours supprimés"
+# fi
+# 
+# for group in $(ldapsearch -xLLL -h $LDAPIP -b $GROUPSRDN,$BASEDN "(&(objectClass=posixGroup)(!(objectClass=sambaGroupMapping)))" cn | grep "^cn:"  | sed "s/cn: //"); do
+# 
+# 	description=$(ldapsearch -xLLL -h $LDAPIP -b $GROUPSRDN,$BASEDN "(&(objectClass=posixGroup)(!(objectClass=sambaGroupMapping)))" description | grep "^description:"  | sed "s/description: //")
+# 	echo "mappage de $group" #>>/tmp/addposix.ldif
+# 	/usr/share/se3/scripts/group_mapping.sh $group $group $description 
+# done
+
+# mise a jour des parametres caches pour les domaine si besoin ( remplacement confse3.ini )
+echo " Remplacement du fichier confse3.ini par des parametres dans la base se3db"
+
+if [ -z "$se3_domain" ]; then 
+    eval $(grep "workgroup =" /etc/samba/smb.conf | sed "s/ //g")
+    SETMYSQL se3_domain "$workgroup" "Nom du domaine windows" 4
+fi
+if [ -z "$netbios_name" ]; then 
+    eval $(grep "netbios name =" /etc/samba/smb.conf | sed "s/ //g")
+    SETMYSQL netbios_name "$netbiosname" "Nom netbios du serveur"4
+    netbios_name=$netbiosname
+fi
+if [ -z "$se3ip" ]; then 
+    se3ip=$(nmblookup $netbios_name | grep "$netbios_name<00>" | cut -d " " -f1)
+    SETMYSQL se3ip "$se3ip" "Adresse IP du serveur" 4
+fi
+
+
+echo "mise en place des privileges samba"
+echo "mise a jour de confse3.ini"
+# 
+CONFSE3="/var/se3/Progs/install/installdll/confse3.ini"
+CONFSE3_SAV="/var/se3/Progs/install/installdll/confse3.ini-root"
+
+# Sauvegarde fichier avant modif si besoin et init variable smbpass
+if [ -f /var/se3/Progs/install/installdll/confse3.ini-root ]; then
+    SMBPASS=`grep "password_ldap_domain" "$CONFSE3_SAV" | sed -e 's/^$//'| sed -e 's/\r//' |cut -d= -f2`
+    net -U adminse3%"$SMBPASS" rpc rights grant adminse3 SeMachineAccountPrivilege SePrintOperatorPrivilege
+else
+
+	ldapsearch -xLLL cn=root -b $BASEDN cn | grep "cn=root" > /dev/null 2>&1 || \
+	echo "dn: cn=root,$BASEDN
+cn: root
+description: compte root samba 
+gecos: samba root  account
+loginShell: /bin/false
+sambaAcctFlags: [NU         ]
+sambaPwdMustChange: 2147483647
+sambaPwdCanChange: 0
+sambaKickoffTime: 2147483647
+sambaLogoffTime: 2147483647
+sambaLogonTime: 0
+sambaPwdLastSet: 0
+homeDirectory: /dev/null
+objectClass: inetOrgPerson
+objectClass: sambaSamAccount
+objectClass: posixAccount
+objectClass: shadowAccount
+objectClass: organizationalPerson
+objectClass: top
+objectClass: person
+uidNumber: 0
+uid: root
+sn: root
+gidNumber: 0
+sambaPrimaryGroupSID: $DOMAINSID-0
+sambaSID: $DOMAINSID-1" | ldapadd -x -h $LDAPIP -D $ADMINRDN,$BASEDN -w $ADMINPW 
+
+    SMBPASS=`grep "password_ldap_domain" "$CONFSE3" | sed -e 's/^$//'| sed -e 's/\r//' |cut -d= -f2`
+    # Creation compte adminse3 dans annuaire si besoin est 
+    [ -z "$(ldapsearch -xLLL uid=adminse3)" ] && /usr/share/se3/sbin/create_adminse3.sh
+    net -U root%"$SMBPASS" rpc rights grant admin SeMachineAccountPrivilege SePrintOperatorPrivilege
+    net -U root%"$SMBPASS" rpc rights grant adminse3 SeMachineAccountPrivilege SePrintOperatorPrivilege
+    if [ $? -eq 0 ]; then 
+	  cp $CONFSE3 $CONFSE3_SAV
+	  sed -i  "s/compte_ldap_domain=root/compte_ldap_domain=adminse3/" $CONFSE3
+	  sed -i "/password_ldap_domain=/d" $CONFSE3 
+	  echo -e "password_ldap_domain=$xppass\r" >> $CONFSE3
+	  ldapdelete -x -h $LDAPIP -D $ADMINRDN,$BASEDN -w $ADMINPW cn=root,$BASEDN
+	  echo "Attention, la mise au domaine se fait maintenant avac le compte adminse3.
+Le compte root samba est maintenant desactive"
+	  echo "ATTENTION A VERIFIER
+la mise en pace des privileges d administration peut causer des problemes 
+avec les pilotes d imprimantes si vous constatez que les pilotes ne fonctionnent plus,
+c est qu il faut les uploader a nouveau sur le serveur (voir la procedure de la doc)"
+    fi
+fi
+
+#Maj scripts clients linux
+#### A intégrer dans se3-domain !!! ###
+#echo "Maj des scripts pour clients linux"
+#/usr/share/se3/sbin/create_client_linux.sh >/dev/null 
+#echo "Les scripts clients linux sont disponibles dans /root
+#Voir la documentation pour plus d'infos"
+
+# on finit par l'actualisation du cache des parametres : 
+
+/usr/share/se3/includes/config.inc.sh -clpbmsdf 
+
+
+echo "Mise a jour 120:
 - Ajout nouvelle architecture de connexion
 - Ajout adminse3 dans l'annuaire par defaut
 - Ajout corbeille reseau
-- Ajout integration vista" >> $HISTORIQUE_MAJ
+- Ajout integration vista
+- Ajout des groupes Domain Users et Domain Guests
+- Modification du sambaPrimaryGroupSID
+- ajout des privileges mise au domaine adminse3, abandon de root samba
+- Migration vers nouveau code de gestion de restrictions
+- correction des scripts imprimantes pour ne donner les droits que pour adminse3" >> $HISTORIQUE_MAJ
 MAIL_REPORT
 echo "Un mail recapitulatif de la mise à jour sera envoyé"
 
 exit 0
-
