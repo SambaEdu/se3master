7da3df87718c90b36e8e8340d12dea4c7d5cf9c6
keyser <keyser@033478fe-9347-4027-94cf-8a8f83edcaf9>
2010-01-31 16:52:50 +0000

    harmonisation droits et lancement depuis permse3 en arriere plan

--- usr/share/se3/sbin/droit_backuppc.sh
+++ usr/share/se3/sbin/droit_backuppc.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
+## $Id$ ##
+#
+##### Permet de positionner les droits pour backuppc #####
+#
 
-if [ -e /var/www/se3/includes/config.inc.php ]
+if [ "$1" = "--help" -o "$1" = "-h" ]
 then
-        dbhost=`cat /var/www/se3/includes/config.inc.php | grep "dbhost=" | cut -d = -f 2 |cut -d \" -f 2`
-        dbname=`cat /var/www/se3/includes/config.inc.php | grep "dbname=" | cut -d = -f 2 |cut -d \" -f 2`
-        dbuser=`cat /var/www/se3/includes/config.inc.php | grep "dbuser=" | cut -d = -f 2 |cut -d \" -f 2`
-        dbpass=`cat /var/www/se3/includes/config.inc.php | grep "dbpass=" | cut -d = -f 2 |cut -d \" -f 2`
-else
-        echo "impossible d'acceder aux params mysql"
-        exit 1
-fi
+	echo "Script permettant de positionner les droits pour backuppc."
+	
+	echo "Usage : pas d'option"
+	exit
+fi	
 
-bck_user=`mysql se3db -u $dbuser -p$dbpass -B -N -e "select value from params where name='bck_user'"`
+bck_user="backuppc"
 
+chown -R www-se3.backuppc /usr/share/backuppc
 chown -R $bck_user.www-data /etc/backuppc
 chmod -R 770 /etc/backuppc
 chown $bck_user.www-data /etc/SeConfig.ph
