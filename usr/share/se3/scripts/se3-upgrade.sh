9be63624aa0b9e6fd8259a845356ce0cb0ae420f
keyser <keyser@033478fe-9347-4027-94cf-8a8f83edcaf9>
2010-02-05 23:45:16 +0000

    ajout prise en compte se3-domain

--- usr/share/se3/scripts/se3-upgrade.sh
+++ usr/share/se3/scripts/se3-upgrade.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
-apt-get update >/dev/null 2>&1
+#
+## $Id$ ##
+#
+##### Permet de faire la mise à jour de se3 et modules #####
+
+# 
+
+if [ "$1" = "--help" -o "$1" = "-h" ]
+then
+        echo "Script permettant la mise a jour de se3 et de ses modules"
+        echo "Usage : sans option "
+        exit
+fi	
+
+echo "<pre>"
+echo "Mise a jour de la liste des paquets disponibles ....."
+apt-get -qq update
 (
 dpkg -l|grep se3|cut -d ' ' -f3|while read package
 do
@@ -7,15 +23,19 @@ LC_ALL=C apt-get -s install $package|grep newest >/dev/null|| echo $package
 done
 )>/tmp/se3_update_list
 
-echo "<br/>"
-LC_ALL=C apt-get install $(cat /tmp/se3_update_list) --allow-unauthenticated -y -o Dpkg::Options::=--force-confold >/tmp/se3_update_mail 2>&1
+#echo "<br/>"
+LC_ALL=C apt-get -s install se3-domain 2>/dev/null | grep 'Inst' && apt-get install se3-domain --allow-unauthenticated -y | tee -a /tmp/se3_update_mail 
+LC_ALL=C apt-get install $(cat /tmp/se3_update_list) --allow-unauthenticated -y -o Dpkg::Options::=--force-confold 2>&1 | tee -a /tmp/se3_update_mail 
 if [ "$?" == "0" ]
 then
-	echo "Mise a jour ok!<br/>"
+	echo "</pre>"
+	echo "Mise a jour ok !<br/>"
 else
-	echo "Mise a jour non ok!<br/>"
+	echo "</pre>"
+	echo "Mise a jour non ok !<br/>"
 fi
-cat /tmp/se3_update_mail | mail -s "[SE3] Résultat de la mise à jour" root
+
+cat /tmp/se3_update_mail | mail -s "[SE3] Resultat de la mise a jour" root
 rm -f /tmp/se3_update_mail
 rm -f /tmp/se3_update_list
 rm -f /etc/se3/update_available
