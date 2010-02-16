87625bb7cffe74debf050b9b6f7052dd6e325ae4
keyser <keyser@033478fe-9347-4027-94cf-8a8f83edcaf9>
2010-02-16 13:49:22 +0000

    utilisation de config.inc.sh et remplacement du separateur dans le sed

--- trunk/se3master/usr/share/se3/sbin/update-smbconf.sh
+++ trunk/se3master/usr/share/se3/sbin/update-smbconf.sh
@@ -1,34 +1,22 @@
 #!/bin/bash
 # Update smb.conf based on current template version and current logon script (pl, py)
 # Keep user defined shares
-
-# Get mysql conf
-dbhost=$(expr "$(grep mysqlServerIp /etc/SeConfig.ph)" : ".*'\(.*\)'.*")
-dbuser=$(expr "$(grep mysqlServerUsername /etc/SeConfig.ph)" : ".*'\(.*\)'.*")
-dbpass=$(expr "$(grep mysqlServerPw /etc/SeConfig.ph)" : ".*'\(.*\)'.*")
-dbname=$(expr "$(grep connexionDb /etc/SeConfig.ph)" : ".*'\(.*\)'.*")
+. /usr/share/se3/includes/config.inc.sh -cml
+#. /usr/share/se3/includes/functions.inc.sh
 
 [ ! -d /home/profiles ] && mkdir /home/profiles
 chown root.root /home/profiles
 chmod 777 /home/profiles
+[ -z "$se3_domain" ] && se3_domain=$(grep "workgroup" /etc/samba/smb.conf|cut -d '=' -f2|sed -e 's/ //g')
+[ -z "$netbios_name" ] && netbios_name=$(grep "netbios name" /etc/samba/smb.conf|cut -d '=' -f2|sed -e 's/ //g')
+[ -z "$se3ip" ] && se3ip="$(expr "$(LC_ALL=C /sbin/ifconfig eth0 | grep 'inet addr')" : '.*inet addr:\([^ ]*\)')"
 
-NTDOM=$(grep "workgroup" /etc/samba/smb.conf|cut -d '=' -f2|sed -e 's/ //g')
-NETBIOS=$(grep "netbios name" /etc/samba/smb.conf|cut -d '=' -f2|sed -e 's/ //g')
-SE3IP="$(expr "$(LC_ALL=C /sbin/ifconfig eth0 | grep 'inet addr')" : '.*inet addr:\([^ ]*\)')"
-MASK="$(expr "$(LC_ALL=C /sbin/ifconfig eth0 | grep Mask)" : '.*Mask:\(.*\)')"
-LDAPIP=`echo "SELECT value FROM params WHERE name='ldap_server'" | mysql -h $dbhost $dbname -u $dbuser -p$dbpass -N`
-BASEDN=`echo "SELECT value FROM params WHERE name='ldap_base_dn'" | mysql -h $dbhost $dbname -u $dbuser -p$dbpass -N`
-ADMINRDN=`echo "SELECT value FROM params WHERE name='adminRdn'" | mysql -h $dbhost $dbname -u $dbuser -p$dbpass -N`
-COMPUTERSRDN=`echo "SELECT value FROM params WHERE name='computersRdn'" | mysql -h $dbhost $dbname -u $dbuser -p$dbpass -N`
-PEOPLERDN=`echo "SELECT value FROM params WHERE name='peopleRdn'" | mysql -h $dbhost $dbname -u $dbuser -p$dbpass -N`
-GROUPSRDN=`echo "SELECT value FROM params WHERE name='groupsRdn'" | mysql -h $dbhost $dbname -u $dbuser -p$dbpass -N`
-REPLICA_STATUS=`echo "SELECT value FROM params WHERE name='replica_status'" | mysql -h $dbhost $dbname -u $dbuser -p$dbpass -N`
-CORBEILLE=`echo "SELECT value FROM params WHERE name='corbeille'" | mysql -h $dbhost $dbname -u $dbuser -p$dbpass -N`
+MASK=$(grep netmask  /etc/network/interfaces | head -n1 | sed -e "s/netmask//g" | tr "\t" " " | sed -e "s/ //g")
 CHARSET=$(grep "unix charset" /etc/samba/smb.conf | cut -d"=" -f2 | sed -e "s/ //")
 [ -z "$CHARSET" ] && CHARSET="UTF-8"
 
 cp -f /etc/samba/smb.conf /tmp
-sed -e "s/#DOMAIN#/$NTDOM/g;s/#NETBIOSNAME#/$NETBIOS/g;s/#IPSERVEUR#/$SE3IP/g;s/#MASK#/$MASK/g;s/#SLAPDIP#/$LDAPIP/g;s/#BASEDN#/$BASEDN/g;s/#ADMINRDN#/$ADMINRDN/g;s/#COMPUTERS#/$COMPUTERSRDN/g;s/#PEOPLE#/$PEOPLERDN/g;s/#GROUPS#/$GROUPSRDN/g;s/#CHARSET#/$CHARSET/g" /var/cache/se3_install/conf/smb_3.conf.in >/etc/samba/smb.conf
+sed -e "s/#DOMAIN#/$se3_domain/g;s/#NETBIOSNAME#/$netbios_name/g;s/#IPSERVEUR#/$se3ip/g;s/#MASK#/$MASK/g;s/#SLAPDIP#/$ldap_server/g;s/#BASEDN#/$ldap_base_dn/g;s/#ADMINRDN#/$adminRdn/g;s/#COMPUTERS#/$computersRdn/g;s/#PEOPLE#/$peopleRdn/g;s/#GROUPS#/$groupsRdn/g;s/#CHARSET#/$CHARSET/g" /var/cache/se3_install/conf/smb_3.conf.in >/etc/samba/smb.conf
 size=$(wc -l /tmp/smb.conf |cut -d ' ' -f1)
 line=$(grep -m1 -n '<.*>' /tmp/smb.conf |cut -d ':' -f1)
 if [ "$line" != "" ]
@@ -38,23 +26,23 @@ fi
 rm -f /tmp/smb.conf
 
 SSL="start_tls"
-if [ "$REPLICA_STATUS" = "2" ]
+if [ "$replica_status" = "2" ]
 then
 	SSL="off"
 fi
 # Pas de ssl si le ldap est local
-if [ "$REPLICA_STATUS" == "" -o "$REPLICA_STATUS" = "0" ]
+if [ "$replica_status" == "" -o "$replica_status" = "0" ]
 then	
-	if [ "$LDAPIP" == "$SE3IP" ]
+	if [ "$ldap_server" == "$se3ip" ]
 	then
 		SSL="off"
 	fi
 fi
 
 sed -i "s!ldap ssl.*!ldap ssl = $SSL!" /etc/samba/smb.conf
-if [ "$CORBEILLE" == "0" ]
+if [ "$corbeille" == "0" ]
 then
-	sed -i "s!recycle:exclude=.*!recycle:exclude=\*\.\*!" /etc/samba/smb*.conf
+	sed -i "s/recycle:exclude=.*/recycle:exclude=\*\.\*/" /etc/samba/smb*.conf
 fi
 
 chmod 644 /etc/samba/smb_*
