3d8433f748a7c4302310c2c7adcce34eb5d3848c
keyser <keyser@033478fe-9347-4027-94cf-8a8f83edcaf9>
2010-02-18 13:32:22 +0000

    mis en conformité avec branche 1.50

--- trunk/se3master/var/cache/se3_install/maj/maj120.sh
+++ trunk/se3master/var/cache/se3_install/maj/maj120.sh
@@ -13,12 +13,8 @@ REPORT_FILE=$LOG_DIR/log_maj120
 #date
 LADATE=$(date +%d-%m-%Y)
 
-### recup pass root mysql
-. /root/.my.cnf 2>/dev/null
-. /usr/share/se3/includes/config.inc.sh -lm
-. /usr/share/se3/includes/functions.inc.sh
 
-mysql -f se3db < /var/cache/se3_install/se3db.sql 2>/dev/null
+mysql -u $dbuser -p$dbpass -f se3db < /var/cache/se3_install/se3db.sql 2>/dev/null
 
 
 
@@ -81,72 +77,9 @@ done
 chmod 700 $AT_SCRIPT
 at now +5 minutes -f $AT_SCRIPT >/dev/null
 echo ""
-# echo  "Ajout des groupes Samba3 obligatoires dans LDAP"
-# # mappage de nobody, root, etc...
-# ldapsearch -xLLL "(&(sambaprimarygroupsid=S-1-5-32-546)(uid=nobody)(sambasid=$DOMAINSID-501))" cn | grep nobody > /dev/null 2>&1 || \
-# echo "dn: uid=nobody,$PEOPLERDN,$BASEDN
-# cn: nobody
-# uid: nobody
-# description: samba guest domain account
-# gecos: samba guest domain account
-# loginShell: /bin/false
-# sambaAcctFlags: [NU         ]
-# sambaPwdMustChange: 2147483647
-# sambaPwdCanChange: 0
-# sambaKickoffTime: 2147483647
-# sambaLogoffTime: 2147483647
-# sambaLogonTime: 0
-# sambaPwdLastSet: 0
-# homeDirectory: /dev/null
-# objectClass: inetOrgPerson
-# objectClass: sambaSamAccount
-# objectClass: posixAccount
-# objectClass: shadowAccount
-# objectClass: organizationalPerson
-# objectClass: top
-# objectClass: person
-# sn: nobody
-# gidNumber: 65534
-# sambaPrimaryGroupSID: S-1-5-32-546
-# uidNumber: 65534
-# sambaSID: $DOMAINSID-501" | ldapadd -x -h $LDAPIP -D $ADMINRDN,$BASEDN -w $ADMINPW
-# 
-# ldapsearch -xLLL "(&(sambasid=S-1-5-32-546)(objectclass=posixgroup))" cn | grep nogroup > /dev/null 2>&1 || \
-# echo "dn: cn=nogroup,$GROUPSRDN,$BASEDN
-# sambaGroupType: 2
-# description: le groupe fantome
-# objectClass: posixGroup
-# objectClass: top
-# objectClass: sambaGroupMapping
-# gidNumber: 65534
-# sambaSID: S-1-5-32-546
-# displayName: nogroup
-# cn: nogroup
-# memberUid: nobody" | ldapadd -x -h $LDAPIP -D $ADMINRDN,$BASEDN -w $ADMINPW 
-# 
-# ldapsearch -xLLL "cn=root -b $GROUPSRDN,$BASEDN" cn | grep root > /dev/null 2>&1 || \
-# echo "dn: cn=root,$GROUPSRDN,$BASEDN
-# objectClass: posixGroup
-# objectClass: sambaGroupMapping
-# gidNumber: 0
-# cn: root
-# description: groupe root samba
-# memberUid: root
-# sambaGroupType: 4
-# sambaSID: $DOMAINSID-0
-# displayName: Roots" | ldapadd -x -h $LDAPIP -D $ADMINRDN,$BASEDN -w $ADMINPW
-# 
-# ldapsearch -xLLL "(&(sambasid=$DOMAINSID-514)(objectclass=posixgroup))" cn | grep invites > /dev/null 2>&1 || \
-# echo "dn: cn=invites,$GROUPSRDN,$BASEDN
-# objectClass: posixGroup
-# objectClass: sambaGroupMapping
-# gidNumber: 999
-# cn: invites
-# description: Invites du domaine
-# memberUid: nobody
-# sambaGroupType: 4
-# sambaSID: $DOMAINSID-514
-# displayName: Invites" | ldapadd -x -h $LDAPIP -D $ADMINRDN,$BASEDN -w $ADMINPW 
+
+echo "maj supervision rouen" 
+./depmaj/install_supervision_rouen.sh
 
 
 # mappage des groupes particuliers
@@ -214,59 +147,20 @@ gidNumber: 0
 sambaPrimaryGroupSID: $DOMAINSID-0
 sambaSID: $DOMAINSID-1" | ldapadd -x -h $LDAPIP -D $ADMINRDN,$BASEDN -w $ADMINPW 
 
+# Creation compte adminse3 dans annuaire si besoin et application provileges smbpour admin et adminse3 
+/usr/share/se3/sbin/create_adminse3.sh
 
-# Sauvegarde fichier avant modif si besoin et init variable smbpass
-#if [ -f /var/se3/Progs/install/installdll/confse3.ini-root ]; then
-smbpasswd -e root
-echo -e "$xppass\n$xppass"|(/usr/bin/smbpasswd -s root)
-SMBPASS="$xppass"
-#else
-# SMBPASS=`grep "password_ldap_domain" "$CONFSE3" | sed -e 's/^$//'| sed -e 's/\r//' |cut -d= -f2`
-#fi
-
-# Creation compte adminse3 dans annuaire si besoin est 
-if [ -z "$(ldapsearch -xLLL uid=adminse3)" ]; then  
-	/usr/share/se3/sbin/create_adminse3.sh
-else
-	net -U root%"$SMBPASS" rpc rights grant admin SeMachineAccountPrivilege SePrintOperatorPrivilege
-	net -U root%"$SMBPASS" rpc rights grant adminse3 SeMachineAccountPrivilege SePrintOperatorPrivilege
-fi
-
-if [ $? -eq 0 ]; then 
-	  cp $CONFSE3 $CONFSE3_SAV
-	  sed -i  "s/compte_ldap_domain=root/compte_ldap_domain=adminse3/" $CONFSE3
-	  sed -i "/password_ldap_domain=/d" $CONFSE3 
-	  echo -e "password_ldap_domain=$xppass\r" >> $CONFSE3
-	  smbpasswd -d root
-else
-      echo "ATTENTION la commande visant a remplacer le compte root d integration par aminse3 a echoue
-      le script va utiliser une methode de secours verifier ensuite que tout fonctionne correctement !!!"
-      sleep 2
-      smbpasswd -e root
-      echo -e "$xppass\n$xppass"|(/usr/bin/smbpasswd -s root)
-      SMBPASS="$xppass"
-      net -U root%"$SMBPASS" rpc rights grant admin SeMachineAccountPrivilege SePrintOperatorPrivilege
-      net -U root%"$SMBPASS" rpc rights grant adminse3 SeMachineAccountPrivilege SePrintOperatorPrivilege
-      smbpasswd -d root
-fi
 echo ""
-echo "Attention, la mise au domaine se fait maintenant avac le compte adminse3.
-Le compte root samba est maintenant desactive
-"
-
 echo "ATTENTION A VERIFIER
 la mise en pace des privileges d administration peut causer des problemes 
 avec les pilotes d imprimantes si vous constatez que les pilotes ne fonctionnent plus,
 c est qu il faut les uploader a nouveau sur le serveur (voir la procedure de la doc)
 "
 
-
 # bye bye se3printers
 rm -f /var/se3/Progs/ro/printers/se3printers.bat
 sed -i "/se3printers.bat/d" /home/templates/base/logon.bat
 
-# on finit par l'actualisation du cache des parametres : 
-/usr/share/se3/includes/config.inc.sh -clpbmsdf 
 
 echo "Mise a jour 120:
 - Ajout nouvelle architecture de connexion
@@ -280,7 +174,7 @@ echo "Mise a jour 120:
 - correction des scripts imprimantes pour ne donner les droits que pour adminse3" >> $HISTORIQUE_MAJ
 MAIL_REPORT
 echo "
-Un mail recapitulatif de la mise à jour sera envoye
+Un mail recapitulatif de la mise a jour sera envoye
 "
 
 exit 0
