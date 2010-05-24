82fceca06645b2828a0b608914c4c9a949b47dfc
keyser <keyser@033478fe-9347-4027-94cf-8a8f83edcaf9>
2010-05-24 00:01:22 +0000

    on ne vire pas adminse3 de l'annuaire......

--- trunk/se3master/usr/share/se3/sbin/peopleClean.pl
+++ trunk/se3master/usr/share/se3/sbin/peopleClean.pl
@@ -17,7 +17,7 @@ $res = $lcs_ldap->search(base   => "$peopleDn",
 warn $res->error if $res->code;
 foreach $entry ($res->entries) {
   $uid = $entry->get_value('uid');
-  next if ($uid eq 'admin' or $uid eq 'webmaster.etab' or $uid eq 'wetab' or $uid eq 'etabw' or $uid eq 'spip.manager' or $uid eq 'unattend');
+  next if ($uid eq 'admin' or $uid eq 'adminse3' or $uid eq 'webmaster.etab' or $uid eq 'wetab' or $uid eq 'etabw' or $uid eq 'spip.manager' or $uid eq 'unattend');
   $res = $lcs_ldap->delete("uid=$uid,$peopleDn");
   print "Suppression de l'utilisateur $uid\n";
   warn $res->error if $res->code;
