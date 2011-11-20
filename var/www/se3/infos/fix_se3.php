f295fef7a1a582498a52c77d40f379284b8aef02
keyser <keyser@033478fe-9347-4027-94cf-8a8f83edcaf9>
2011-11-20 13:47:23 +0000

    Ajout de la possibilité d'afficher le mot de passe "adminse3"

--- var/www/se3/infos/fix_se3.php
+++ var/www/se3/infos/fix_se3.php
@@ -28,6 +28,7 @@ require ("entete.inc.php");
 require ("ihm.inc.php");
 
 require_once ("lang.inc.php");
+
 bindtextdomain('se3-infos',"/var/www/se3/locale");
 textdomain ('se3-infos');
 
@@ -48,6 +49,11 @@ if (isset($_GET['action'])) {
         system("sudo /usr/share/se3/scripts/permse3");
         echo "Termin&#233;.";
     }
+    if ($_GET['action'] == "adminse3pass") {
+        echo "<h2>".gettext("Affichage du mot de passe adminse3...")."</h2>";
+        echo "Le mot de passe adminse3 est actuellement <b>$xppass</b>";
+        //echo "Termin&#233;.";
+    }
     if ($_GET['action'] == "restore_droits") {
         echo "<h2>".gettext("Remise en place des droits sur les comptes utilisateurs...")."</h2>";
         system("sudo /usr/share/se3/scripts/restore_droits.sh --home html");
@@ -58,10 +64,12 @@ if (isset($_GET['action'])) {
     }
 }
 else {
+    echo "<a href=\"fix_se3.php?action=adminse3pass\" onClick=\"alert('Vous allez afficher un mot de passe important, attention aux regards indiscrets !!');\">".gettext("Afficher le mot de passe adminse3")."</a>&nbsp;<u onmouseover=\"return escape".gettext("('Effectuez cette action si vous constatez des lenteurs de connexions')")."\"><img name=\"action_image1\"  src=\"../elements/images/system-help.png\"></u><br>";
     echo "<a href=\"fix_se3.php?action=rmprofiles\" onclick=\"return getlongconfirm();\">".gettext("Supprimer l'ensemble des profils Windows")."</a>&nbsp;<u onmouseover=\"return escape".gettext("('Effectuez cette action si vous constatez des lenteurs de connexions')")."\"><img name=\"action_image1\"  src=\"../elements/images/system-help.png\"></u><br>";
     echo "<a href=\"fix_se3.php?action=permse3\" onclick=\"return getlongconfirm();\">".gettext("Remise en place des droits syst&#232;me par d&#233;faut")."</a>&nbsp;<u onmouseover=\"return escape".gettext("('Effectuez cette action si vous constatez des disfonctionnement dans l\'interface ou lors des connexions')")."\"><img name=\"action_image2\"  src=\"../elements/images/system-help.png\"></u><br>";
     echo "<a href=\"fix_se3.php?action=restore_droits\" onclick=\"return getlongconfirm();\">".gettext("Remise en place des droits sur les comptes utilisateurs")."</a>&nbsp;<u onmouseover=\"return escape".gettext("('Effectuez cette action si vous constatez des problemes de droits pour les utilisateurs')")."\"><img name=\"action_image3\"  src=\"../elements/images/system-help.png\"></u><br>";
     echo "<a href=\"fix_se3.php?action=restore_droits_full\" onclick=\"return getlongconfirm();\">".gettext("Remise en place de tous les droits")."</a>&nbsp;<u onmouseover=\"return escape".gettext("('Effectuez cette action si vous constatez des problemes de droits')")."\"><img name=\"action_image4\"  src=\"../elements/images/system-help.png\"></u><br>";
 }
 require ("pdp.inc.php");
+
 ?>
