ba765470cf44e266a90e36df4976984b1b9ac5ae
keyser <keyser@033478fe-9347-4027-94cf-8a8f83edcaf9>
2012-10-19 21:40:59 +0000

    initial release

---
+++ var/cache/se3_install/maj/maj138.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+## $Id:$ ##
+
+# path fichier de logs
+LOG_DIR="/var/log/se3"
+HISTORIQUE_MAJ="$LOG_DIR/historique_maj"
+REPORT_FILE="$LOG_DIR/log_maj138"
+#mode debug on si =1
+[ -e /root/debug ] && DEBUG="1"
+# [ -e /usr/share/se3/scripts/update_droits_xml.sh ] && /usr/share/se3/scripts/update_droits_xml.sh
+#date
+LADATE=$(date +%d-%m-%Y)
+
+echo "Mise a jour 138:
+- Correctif pour mysql sup 5.1.12
+- Suppression de l'utilisation de nscd (doublons uidnumbers) " >> $HISTORIQUE_MAJ
+ 
+echo "- Correctif pour mysql sup 5.1.12
+- Suppression de l'utilisation de nscd (doublons uidnumbers)"
+
+# On assure la comptibilite mysql superieur a 5.1.12
+sed -i 's/^skip-bdb/#skip-bdb/g'  /etc/mysql/my.cnf
+
+# nscd sucks !
+if [ -e /etc/init.d/nscd  ]; then
+	insserv -r nscd
+	/etc/init.d/nscdstop
+fi
+
+exit 0		
