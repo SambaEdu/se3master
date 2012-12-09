b849ac611d2fd9df332bd80c021dac470b0ff82d
keyser <keyser@033478fe-9347-4027-94cf-8a8f83edcaf9>
2012-12-09 18:02:56 +0000

    Correction num version du script

--- var/cache/se3_install/maj/maj141.sh
+++ var/cache/se3_install/maj/maj141.sh
@@ -5,14 +5,14 @@
 # path fichier de logs
 LOG_DIR="/var/log/se3"
 HISTORIQUE_MAJ="$LOG_DIR/historique_maj"
-REPORT_FILE="$LOG_DIR/log_maj138"
+REPORT_FILE="$LOG_DIR/log_maj141"
 #mode debug on si =1
 [ -e /root/debug ] && DEBUG="1"
 # [ -e /usr/share/se3/scripts/update_droits_xml.sh ] && /usr/share/se3/scripts/update_droits_xml.sh
 #date
 LADATE=$(date +%d-%m-%Y)
 
-echo "Mise a jour 138:
+echo "Mise a jour 141:
 - Correctif pour mysql sup 5.1.12
 - Suppression de l'utilisation de nscd (doublons uidnumbers) " >> $HISTORIQUE_MAJ
  
