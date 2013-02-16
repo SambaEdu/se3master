94125ad9cc33856793e8a1a563322956b0e69eec
keyser <keyser@033478fe-9347-4027-94cf-8a8f83edcaf9>
2013-02-16 23:44:36 +0000

    dummie

---
+++ trunk/se3master/var/cache/se3_install/maj/maj140.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+## $Id: maj140.sh 7667 2013-02-16 23:36:35Z keyser $ ##
+
+# path fichier de logs
+LOG_DIR="/var/log/se3"
+HISTORIQUE_MAJ="$LOG_DIR/historique_maj"
+REPORT_FILE="$LOG_DIR/log_maj140"
+#mode debug on si =1
+[ -e /root/debug ] && DEBUG="1"
+
+#date
+LADATE=$(date +%d-%m-%Y)
+
+# Just 	a dummie
+
+exit 0		
+
+
+
+
