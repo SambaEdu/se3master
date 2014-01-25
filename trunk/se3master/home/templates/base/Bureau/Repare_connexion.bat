b7ae8d066345df309d607d66ac9c5cc7a6a66027
keyser <keyser@033478fe-9347-4027-94cf-8a8f83edcaf9>
2014-01-25 22:57:19 +0000

    Prise en compte bug seven concernant lecteurs inaccessibles

--- trunk/se3master/home/templates/base/Bureau/Repare_connexion.bat
+++ trunk/se3master/home/templates/base/Bureau/Repare_connexion.bat
@@ -1,3 +1,13 @@
+VER | FINDSTR /L "5.1." > NUL
+IF %ERRORLEVEL% EQU 0 goto xp
+
+NET USE H: /DELETE /YES 
+NET USE I: /DELETE /YES 
+NET USE L: /DELETE /YES 
+if exist X:\ NET USE X: /DELETE /YES 
+if exist Y:\ NET USE Y: /DELETE /YES 
+
+:xp 
 
 if not exist I:\ net use I: \\se3\Docs
 if not exist H:\ net use H: \\se3\Classes
@@ -5,6 +15,8 @@ if not exist L:\ net use L: \\se3\Progs
 echo %USERNAME
 if %USERNAME% == admin goto admin
 goto imp
+
+
 
 :admin
 
@@ -14,3 +26,6 @@ if not exist Y:\ net use Y: \\se3\admse3
 pause
 :imp
 start /B cscript //B %SYSTEMROOT%\Printers.vbs
+
+
+
