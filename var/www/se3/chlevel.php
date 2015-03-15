72198575080dd00d7eab1d02894a0285a9d21527
plouf <plouf@033478fe-9347-4027-94cf-8a8f83edcaf9>
2015-03-15 12:05:26 +0000

    Passage UTF 8

--- var/www/se3/chlevel.php
+++ var/www/se3/chlevel.php
@@ -8,7 +8,7 @@
    * @Projet LCS / SambaEdu 
    
    * @auteurs  jLCF >:>  jean-luc.chretien@tice.ac-caen.fr
-   * @auteurs « oluve » olivier.le_monnier@crdp.ac-caen.fr
+   * @auteurs  oluve olivier.le_monnier@crdp.ac-caen.fr
    * @auteurs Olivier LECLUSE
 
    * @Licence Distribue selon les termes de la licence GPL
