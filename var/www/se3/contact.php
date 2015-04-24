fbc4ed61a9319d0179e83e0623e63ada9f29a07e
keyser <keyser@033478fe-9347-4027-94cf-8a8f83edcaf9>
2015-04-24 08:14:58 +0000

    Mise à jour infos et correction accents

--- var/www/se3/contact.php
+++ var/www/se3/contact.php
@@ -36,8 +36,8 @@ $texte=gettext("<P>SambaEdu3 est distribu&#233; selon <A HREF='http://www.gnu.or
 mktable (gettext("A propos"), "<DIV ALIGN='center'>SambaEdu-3 Version $version</DIV>".$texte);
 print "<BR>";
 
-$texte = gettext("<P>Ce projet a &#233;t&#233; initi&#233; par le <A HREF='http://www.crdp.ac-caen.fr' TARGET='_new'>CRDP de Basse-Normandie</A> et est � pr&#233;sent port&#233; par des contributeurs de plusieurs acad&#233;mies : Rouen, Creteil, Versailles, Grenoble, Lyon... 
-<P>Pour tout renseignement, vous pouvez contacter <A HREF='mailto:olivier.lecluse@crdp.ac-caen.fr'>Olivier LECLUSE</A>, chef du projet SambaEdu3.</P>
+$texte = gettext("<P>Ce projet a &#233;t&#233; initi&#233; par le <A HREF='http://www.crdp.ac-caen.fr' TARGET='_new'>CRDP de Basse-Normandie</A> et est &#224; pr&#233;sent port&#233; par des contributeurs de plusieurs acad&#233;mies : Rouen, Creteil, Versailles, Grenoble, Lyon... 
+<P>Pour tout renseignement, vous pouvez contacter <A HREF='mailto:olivier.lecluse1@ac-caen.fr'>Olivier LECLUSE</A>, chef du projet SambaEdu3.</P>
 <P>Un grand merci &#224; tous ceux qui contribuent au d&#233;veloppement, aux tests, ou &#224; la documentation du projet, en particulier, pour l'Acad&#233;mie de Caen</P>
 <UL>
 <LI><A HREF='mailto:oluve@crdp.ac-caen.fr'>Olivier Le monnier</A> qui a d&#233;velopp&#233; la structure de l'annuaire, l'API perl et le l&#233;gendaire gep.cgi</LI>
@@ -56,7 +56,13 @@ $texte = gettext("<P>Ce projet a &#233;t&#233; initi&#233; par le <A HREF='http:
 <LI>Olivier LeCam a l'origine de l'installateur automatique <EM>DiGloo</EM>
 <LI>Sandrine Dangreville pour l'interface de gestion des cles de registe windows
 <LI>Jean Gourdin pour l'interface de remise de devoirs
-<LI>Olivier Lacroix pour l'interface quotas
+<LI>Olivier Lacroix pour l'interface quotas et tout le travail autour de wpkg
+<LI>Louis Maurice de Sousa et Francois Lafont pour le support des clients Linux
+</UL>
+
+<P>pour l'acad&#233;mie de Paris
+<UL>
+<LI> Denis Bonnenfant pour son expertise, toutes ses contributions (se3-internet, se3-synchro, se3-domain) et la veille au quotidien du projet
 </UL>
 <P>pour le Carip de l'Acad&#233;mie de Lyon
 <UL>
@@ -72,9 +78,8 @@ $texte = gettext("<P>Ce projet a &#233;t&#233; initi&#233; par le <A HREF='http:
 </UL>
 <P>pour l'acad&eacute;mie de Rouen
 <UL>
-<LI><A HREF='mailto:Stephane.boireau@ac-rouen.fr'>St&eacute;phane Boireau </A>pour le module clonage / TFTP et l'import des comptes depuis sconet
-<LI><A HREF='mailto:franck.molle@ac-rouen.fr'>Franck Molle </A>pour le suivi au quotidien du projet, la mise � jour de l'installeur Digloo, des scripts d'installation et de mise � jour...
-haut vol ...
+<LI><A HREF='mailto:Stephane.boireau@ac-rouen.fr'>St&eacute;phane Boireau </A>pour le module clonage / TFTP et l'import des comptes depuis SIECLE / STS
+<LI><A HREF='mailto:franck.molle@ac-rouen.fr'>Franck Molle </A>pour le suivi au quotidien du projet, la mise &#224; jour de l'installeur, des scripts d'installation et de mise &#224; jour.
 </UL>
 <P>Et tous les autres contributeurs au projet, qu'ils soient d&#233;veloppeurs, testeurs ou r&#233;dacteurs sur la documentation.
 <P>SambaEdu est un produit libre et ind&#233;pendant, d&#233;velopp&#233; par des professeurs b&#233;n&#233;voles pour l'ensemble de la communaut&#233; &#233;ducative. C'est un projet en constante &#233;volution depuis 2000 et qui est officiellement support&#233; et d&#233;ploy&#233; dans plusieurs acad&#233;mies comme Caen, Rouen, Versailles, Strasbourg, Nantes, Clermont ferrand ...  
