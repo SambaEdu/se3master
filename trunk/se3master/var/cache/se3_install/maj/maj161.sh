2caa1060b9a13b71bc3aea4a9cb8ecb6bab18eea
keyser <keyser@033478fe-9347-4027-94cf-8a8f83edcaf9>
2015-04-26 20:19:14 +0000

    correction version script maj

--- trunk/se3master/var/cache/se3_install/maj/maj161.sh
+++ trunk/se3master/var/cache/se3_install/maj/maj161.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-## $Id: maj160.sh 7589 2013-01-06 01:11:10Z keyser $ ##
+## $Id: maj161.sh 7589 2013-01-06 01:11:10Z keyser $ ##
 
 # path fichier de logs
 LOG_DIR="/var/log/se3"
 HISTORIQUE_MAJ="$LOG_DIR/historique_maj"
-REPORT_FILE="$LOG_DIR/log_maj160"
+REPORT_FILE="$LOG_DIR/log_maj161"
 #mode debug on si =1
 [ -e /root/debug ] && DEBUG="1"
 LADATE=$(date +%d-%m-%Y)
 
 
-echo "Mise a jour 160 :
+echo "Mise a jour 161 :
 - Mise en place nouvelle structure sudoers"
 
 if [ -e /etc/sudoers.dpkg-dist ]; then
@@ -19,14 +19,8 @@ if [ -e /etc/sudoers.dpkg-dist ]; then
   mv /etc/sudoers.dpkg-dist /etc/sudoers && echo "/etc/sudoers.dpkg-dist restauré"
 fi
  
-
-
-
-echo "Mise a jour 160 :
+echo "Mise a jour 161 :
 - Mise en place nouvelle structure sudoers">> $HISTORIQUE_MAJ
  
-
-echo "Mise a jour 160 :
-- Mise en place nouvelle structure sudoers"
 
 exit 0		
