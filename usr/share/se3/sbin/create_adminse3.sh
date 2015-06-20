731ad02b78e9627d10c8821e8dfea44c74d02835
keyser <keyser@033478fe-9347-4027-94cf-8a8f83edcaf9>
2015-06-20 16:48:58 +0000

    Suppression accents et ajout exit 0  en fin de script

--- usr/share/se3/sbin/create_adminse3.sh
+++ usr/share/se3/sbin/create_adminse3.sh
@@ -12,26 +12,26 @@
 ADMINSE3="adminse3"
 
 if [ "$xppass" == "" ]; then
-   echo "Pas de mot de passe défini pour $ADMINSE3 : le champ 'xppass' de la table params de la base mysql se3db est vide ou absent."
+   echo "Pas de mot de passe defini pour $ADMINSE3 : le champ 'xppass' de la table params de la base mysql se3db est vide ou absent."
    exit 1
 fi   
 if ( getent passwd $ADMINSE3 > /dev/null ) ; then
-   echo "Le compte $ADMINSE3 existe déjà : son mot de passe est mis à jour."
+   echo "Le compte $ADMINSE3 existe deja : son mot de passe est mis a jour."
    /usr/share/se3/sbin/userChangePwd.pl $ADMINSE3 $xppass
 else
    # Creation user adminse3
    if [ "$uidPolicy" != "4" ]; then
      CHANGEMYSQL uidPolicy 4 
    fi
-   # adminse3 c'est un mâle ;-)
+   # adminse3 c'est un male ;-)
    if ( ! /usr/share/se3/sbin/userAdd.pl 3 adminse $xppass 00000000 M Administratifs ) ; then
-      echo "Erreur de création du compte $ADMINSE3"
+      echo "Erreur de creation du compte $ADMINSE3"
    fi
    if [ "$uidPolicy" != "4" ]; then
       CHANGEMYSQL uidPolicy "$uidPolicy"
       # echo "UPDATE params SET value=\"$UIDPOLICY\" WHERE name='uidPolicy'" | mysql -h $dbhost $dbname -u $dbuser -p$dbpass -N
    fi
-   echo "Le compte $ADMINSE3 a été ajouté dans l'annuaire."
+   echo "Le compte $ADMINSE3 a ete ajoute dans l'annuaire."
 fi
 smbpasswd -e root
 echo -e "$xppass\n$xppass"|(/usr/bin/smbpasswd -s root)
@@ -43,7 +43,7 @@ smbpasswd -d root
 passtmp=$(makepasswd)
 echo -e "$passtmp\n$passtmp"|(/usr/bin/smbpasswd -s root) 
 
-
 echo "Attention, la mise au domaine se fait maintenant avac le compte adminse3.
 Le compte root samba est maintenant desactive"
 
+exit 0
