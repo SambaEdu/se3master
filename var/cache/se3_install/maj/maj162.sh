f385d59af76aa760506196fe874260b233f13b3b
keyser <keyser@033478fe-9347-4027-94cf-8a8f83edcaf9>
2015-07-03 09:10:07 +0000

    propset et cp au lieu de mv

--- var/cache/se3_install/maj/maj162.sh
+++ var/cache/se3_install/maj/maj162.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-## $Id: maj162.sh 8745 2015-04-26 20:20:58Z keyser $ ##
+## $Id$ ##
 
 # path fichier de logs
 LOG_DIR="/var/log/se3"
@@ -14,7 +14,7 @@ LADATE=$(date +%d-%m-%Y)
 echo "Mise a jour 162 :
 - Remise en place conf sudo par defaut"
 
-mv  /etc/sudoers.se3 /etc/sudoers
+cp  /etc/sudoers.se3 /etc/sudoers
 chmod 440 /etc/sudoers
 service sudo restart
 
