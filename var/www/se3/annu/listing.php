66f549f0455b38a00b3415693f2bba71b53f499b
plouf <plouf@033478fe-9347-4027-94cf-8a8f83edcaf9>
2015-07-04 06:54:53 +0000

    Passage en UTF-8

--- var/www/se3/annu/listing.php
+++ var/www/se3/annu/listing.php
@@ -95,7 +95,7 @@ usort($listing, "trieleve");
 
 
 	require_once(dirname(__FILE__).'/../html2pdf/html2pdf.class.php');
-	$html2pdf = new HTML2PDF('P','A4','fr', false, 'ISO-8859-15');
+	$html2pdf = new HTML2PDF('P','A4','fr', false, 'UTF-8');
 	$html2pdf->WriteHTML($content);
 	$html2pdf->Output('liste.pdf');
 
