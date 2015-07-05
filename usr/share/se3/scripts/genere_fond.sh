38086211eace3ec574f8a0f5cf080dd77e783e55
plouf <plouf@033478fe-9347-4027-94cf-8a8f83edcaf9>
2015-07-05 14:05:17 +0000

    Passage à UTF-8

--- usr/share/se3/scripts/genere_fond.sh
+++ usr/share/se3/scripts/genere_fond.sh
@@ -2,15 +2,19 @@
 #
 ##### Script de configuration de fonds #####
 #
-# Auteur: Stéphane Boireau (A.S. - Relais de Bernay/Pont-Audemer (27))
+# Auteur: StÃ©phane Boireau (A.S. - Relais de Bernay/Pont-Audemer (27))
 #
+
+
 ## $Id$ ##
+
+
 #
 # /usr/share/se3/scripts/genere_fond.sh
-# Dernière modification: 23/05/2006
+# DerniÃ¨re modification: 23/05/2006
 
 if [ "$1" = "--help" -o "$1" = "-h" ]; then
-    echo "Script permettant de configurer les fonds d'écran..."
+    echo "Script permettant de configurer les fonds d'Ã©cran..."
     echo ""
     echo "Usage : plein d'options (### A PRECISER ###)"
     exit
@@ -31,12 +35,12 @@ COLCMD="\033[1;37m"     # Blanc
 COLERREUR="\033[1;31m"  # Rouge
 COLINFO="\033[0;36m"    # Cyan
 
-# Chemin de stockage des paramètres:
+# Chemin de stockage des paramÃ¨tres:
 chemin_param_fond="/etc/se3/fonds_ecran"
-# On y trouve/génère un fichier de couleurs par défaut.
-# On y précise si tel groupe obtient un fond annoté ou non.
+# On y trouve/gÃ©nÃ¨re un fichier de couleurs par dÃ©faut.
+# On y prÃ©cise si tel groupe obtient un fond annotÃ© ou non.
 
-# Création au besoin du dossier:
+# CrÃ©ation au besoin du dossier:
 mkdir -p "$chemin_param_fond"
 
 # Dossier de stockage des fonds communs:
@@ -55,19 +59,19 @@ nom_du_script=$(basename $0)
 if [ -z "$1" -o ! -e "/usr/bin/convert" ]; then
     echo -e ""
     echo -e "${COLTITRE}INFO:${COLINFO}"
-    echo -e "Ce script permet de générer un ${COLCMD}fond.jpg${COLINFO} lors du login sur SE3."
-    echo -e "Il est possible de définir des fonds différents sont proposés pour:"
+    echo -e "Ce script permet de gÃ©nÃ©rer un ${COLCMD}fond.jpg${COLINFO} lors du login sur SE3."
+    echo -e "Il est possible de dÃ©finir des fonds diffÃ©rents sont proposÃ©s pour:"
     echo -e "   - ${COLCMD}admin${COLINFO}"
     echo -e "   - ${COLCMD}Profs${COLINFO}"
     echo -e "   - ${COLCMD}Eleves${COLINFO}"
     echo -e "   - ${COLCMD}Classe_XXX${COLINFO}"
     echo -e "   - ${COLCMD}Administratifs${COLINFO}"
     echo -e "   - ${COLCMD}overfill${COLINFO}"
-    echo -e "Le fond peut ou non être annoté avec les informations suivantes:"
+    echo -e "Le fond peut ou non Ãªtre annotÃ© avec les informations suivantes:"
     echo -e "  - le nom de l'utilisateur"
-    echo -e "  - le prénom de l'utilisateur"
+    echo -e "  - le prÃ©nom de l'utilisateur"
     echo -e "  - la classe de l'utilisateur"
-    echo -e "L'annotation peut n'être activée que pour certains groupes."
+    echo -e "L'annotation peut n'Ãªtre activÃ©e que pour certains groupes."
     echo -e ""
 
     echo -e "$COLTXT"
@@ -83,12 +87,12 @@ if [ "$t" != "1" ]; then
     exit 0
 fi
 
-# Paramètres communs:
+# ParamÃ©tres communs:
 generer_parametres_generation_fonds="non"
 if [ -e "$chemin_param_fond/parametres_generation_fonds.sh" ]; then
     chmod +x "$chemin_param_fond/parametres_generation_fonds.sh"
 
-    # Récupération des variables communes:
+    # RÃ©cupÃ©ration des variables communes:
     source "$chemin_param_fond/parametres_generation_fonds.sh"
 
     if [ -z "$largeur" ]; then
@@ -99,23 +103,23 @@ fi
 if [ ! -e "$chemin_param_fond/parametres_generation_fonds.sh" -o "$generer_parametres_generation_fonds" = "oui" ]; then
         echo "
 # Dossier contenant les trames
-# (image de fond commune à un des utilisateurs/groupes admin, profs, eleves)
+# (image de fond commune Ã  un des utilisateurs/groupes admin, profs, eleves)
 dossier_base_fond="/var/se3/Docs/media/fonds_ecran"
 mkdir -p ${dossier_base_fond}
 # Woody ou Sarge:
-# Pour Sarge, il faut spécifier 'bmp3:' pour le format BMP
+# Pour Sarge, il faut spÃ©cifier 'bmp3:' pour le format BMP
 prefixe=jpeg:
 
-# Couleurs et dimensions par défaut:
+# Couleurs et dimensions par dÃ©faut:
 largeur=800
 hauteur=600
 couleur1=silver
 couleur2=white
-# Ces valeurs seront outrepassées par les re-définitions ultérieures." >> "$chemin_param_fond/parametres_generation_fonds.sh"
+# Ces valeurs seront outrepassÃ©es par les re-dÃ©finitions ultÃ©rieures." >> "$chemin_param_fond/parametres_generation_fonds.sh"
 
     chmod +x "$chemin_param_fond/parametres_generation_fonds.sh"
 
-    # Récupération des variables communes:
+    # RÃ©cupÃ©ration des variables communes:
     source "$chemin_param_fond/parametres_generation_fonds.sh"
 fi
 
@@ -128,7 +132,7 @@ if [ "$1" = "variable_bidon" ]; then
             else
                 rm -f "${dossier_base_fond}/$3.jpg"
             fi
-            # Il s'agit ici de générer un nouveau fond
+            # Il s'agit ici de gÃ©nÃ©rer un nouveau fond
             source "$chemin_param_fond/parametres_${3}.sh"
         ;;
         "image_fournie")
@@ -136,8 +140,8 @@ if [ "$1" = "variable_bidon" ]; then
             if [ -e "$dossier_upload_images/$3.jpg" ]; then
                 rm  -f "${dossier_base_fond}/$3.jpg"
 
-                # Lorsque l'image est uploadée, le nom est forcé à $groupe.jpg,
-                # même si l'image n'est pas de type JPG
+                # Lorsque l'image est uploadÃ©e, le nom est forcÃ© Ã  $groupe.jpg,
+                # mÃªme si l'image n'est pas de type JPG
                 # Les lignes qui suivent assurent la conversion.
                 if ! file $dossier_upload_images/$3.jpg | grep "JPEG" > /dev/null; then
                     mv $dossier_upload_images/$3.jpg $dossier_upload_images/$3.tmp
@@ -154,8 +158,8 @@ if [ "$1" = "variable_bidon" ]; then
             fi
             #source "$chemin_param_fond/annotations_${3}.sh"
             temoin=""
-            # NOTE: Dans le cas où $1=variable_bidon, on se moque des annotations...
-            #       Et si le fichier annotations_${3}.sh n'existe pas, le script s'arrête là sur une erreur.
+            # NOTE: Dans le cas oÃ¹ $1=variable_bidon, on se moque des annotations...
+            #       Et si le fichier annotations_${3}.sh n'existe pas, le script s'arrÃªte lÃ  sur une erreur.
         ;;
 	"supprimer")
 		for file in /var/se3/Docs/media/fonds_ecran/[a-z]*.jpg
@@ -177,8 +181,8 @@ if [ "$1" = "variable_bidon" ]; then
 	;;
 	"genere_base")
 
-		# Ou alors, mettre un témoin dans Consultation des paramétrages pour annoncer que tant que personne ne s'est connecté, on n'a pas le modèle?
-		# C'est embêtant pour faire les réglages.
+		# Ou alors, mettre un tÃ©moin dans Consultation des paramÃ©trages pour annoncer que tant que personne ne s'est connectÃ©, on n'a pas le modÃ¨le?
+		# C'est embÃ©tant pour faire les rÃ©glages.
 
 		temoin=""
 
@@ -189,7 +193,7 @@ if [ "$1" = "variable_bidon" ]; then
 
 		t=$(echo "$3"|sed -e "s/[A-Za-z0-9_]//g")
 		if [ -n "$t" ]; then
-			echo "ERREUR : La chaine '$3' comporte des caratères invalides: '$t'"
+			echo "ERREUR : La chaine '$3' comporte des caratÃ¨res invalides: '$t'"
 			exit
 		fi
 
--- usr/share/se3/scripts/getfacl.sh
+++ usr/share/se3/scripts/getfacl.sh
@@ -1,5 +1,10 @@
 
 #!/bin/bash
+
+
+## $Id$ ##
+
+
 # lire les acls
 
 getfacl "$1" > /tmp/test.tmp
