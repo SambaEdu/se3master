ac84255db75d987bbb1ec21318830319d27cc21b
mollef <franck.molle@ac-rouen.fr>
2016-02-29 01:40:33 +0100

    Récupération pass root sql ajouts messages en cas de pb, Ajout de commentaires, redirection sortie et restart mysql

--- var/cache/se3_install/maj/maj163.sh
+++ var/cache/se3_install/maj/maj163.sh
@@ -11,14 +11,16 @@ REPORT_FILE="$LOG_DIR/log_maj163"
 LADATE=$(date +%d-%m-%Y)
 
 SQL_DUMP="/var/cache/se3_install/se3db-utf8.sql"
+PASS_SQL="$(grep -vE '^[[:space:]]*#' /root/.my.cnf | grep password /root/.my.cnf | cut -d "=" -f2)"
+exec 2>&1
 
 echo "Mise a jour 163 :
-- ajout tache cron effaceemnt des profiles
+- ajout tache cron effacement des profiles
 - Migration de la bdd en utf8
 "
 
 echo "Mise a jour 163 :
-- ajout tache cron effaceemnt des profiles
+- ajout tache cron effacement des profiles
 - Migration de la bdd en utf8
 ">> $HISTORIQUE_MAJ
  
@@ -26,11 +28,36 @@ echo "Mise a jour 163 :
 # echo "# effacement des profils a effacer toutes les 2 minutes
 # */2 * * * * root /usr/share/se3/sbin/clean_profiles.sh" >> /etc/cron.d/se3
 
-mysqldump -c -e --default-character-set=utf8 --single-transaction --skip-set-charset --add-drop-database -B se3db > "$SQL_DUMP" 
+mysqldump -u root -p$PASS_SQL -c -e --default-character-set=utf8 --single-transaction --skip-set-charset --add-drop-database -B se3db > "$SQL_DUMP" 
+if [ "$?" = "0" ];then
+	echo "Extraction de la base sur $SQL_DUMP Ok !"
+	cp $SQL_DUMP /var/cache/se3_install/se3db-iso.sql
+	sed -i 's/DEFAULT CHARACTER SET latin1/DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci/' "$SQL_DUMP" 
+	sed -i 's/DEFAULT CHARSET=latin1/DEFAULT CHARSET=utf8/' "$SQL_DUMP" 
+	
+	mysql -u root -p$PASS_SQL se3db < "$SQL_DUMP" 
+	if [ "$?" = "0" ];then
+		echo "Import du fichier $SQL_DUMP au format utf8 ok"
+		/etc/init.d/mysql restart
 
-sed -i 's/DEFAULT CHARACTER SET latin1/DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci/' "$SQL_DUMP" 
-sed -i 's/DEFAULT CHARSET=latin1/DEFAULT CHARSET=utf8/' "$SQL_DUMP" 
+	else
+		echo "Import du fichier $SQL_DUMP au format utf8 en echec !"
+	fi	
+	
+else
+	echo "Extraction de la base sur $SQL_DUMP en echec !"
+	echo "la commande suivante a échouée" 
+	echo "mysqldump -u root -p$PASS_SQL -c -e --default-character-set=utf8 --single-transaction --skip-set-charset --add-drop-database -B se3db"
+	echo "pour faire la bascule UTF manuellement :
+	- extraire la base avec mysqldump > export.sql
+	- sed -i 's/DEFAULT CHARACTER SET latin1/DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci/' export.sql
+	- sed -i 's/DEFAULT CHARSET=latin1/DEFAULT CHARSET=utf8/' export.sql
+	- réimporter avec mysql -u root -p$PASS_SQL se3db < export.sql
+	- Le pass root sql se trouve dans /root/.my.cnf"
+fi
 
-mysql se3db < "$SQL_DUMP" 
+
+
+
 
 exit 0
