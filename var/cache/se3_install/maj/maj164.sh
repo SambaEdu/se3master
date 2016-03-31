0c0357e8f77c4af02d558cac62b6e90f03c2f394
mollef <franck.molle@ac-rouen.fr>
2016-03-31 17:14:10 +0200

    modif sql

--- var/cache/se3_install/maj/maj164.sh
+++ var/cache/se3_install/maj/maj164.sh
@@ -9,7 +9,7 @@ REPORT_FILE="$LOG_DIR/log_maj164"
 #mode debug on si =1
 [ -e /root/debug ] && DEBUG="1"
 LADATE=$(date +%d-%m-%Y)
-
+PASS_SQL="$(grep -vE '^[[:space:]]*#' /root/.my.cnf | grep password /root/.my.cnf | cut -d "=" -f2)"
 exec 2>&1
 
 cat >/etc/apt/sources.list.d/se3.list <<END
@@ -23,6 +23,10 @@ deb http://wawadeb.crdp.ac-caen.fr/debian wheezy se3
 deb http://wawadeb.crdp.ac-caen.fr/debian wheezybackports smb41
 END
 
+if  [ -e /var/www/se3/dhcp ];then
+	mysql -u root -p$PASS_SQL -D se3db -e "UPDATE params SET descr='Début de la plage de reservation (par defaut network + 51)' WHERE name='dhcp_ip_min';"
+
+fi
 
 exec 2>&1
 
