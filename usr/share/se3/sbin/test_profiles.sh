36a3437a2b7d0b5e62511027188668508e69d4e9
lolo4992 <laurentjoly@orange.fr>
2016-04-03 03:29:50 +0200

    optimisation et nettoyage

--- usr/share/se3/sbin/test_profiles.sh
+++ usr/share/se3/sbin/test_profiles.sh
@@ -11,33 +11,36 @@ chown -R root:root /home/netlogon/delProfile2.txt;
 chmod 755 /home/netlogon/delProfile2.txt;
 
 echo "Debut de test des profils";
-for line in /home/profiles/*.V2; do
-line2=${line:15:$((${#line}-18))};
-if [ -f "$line/NTUSER.DAT" ] && [ -f "$line/ntuser.ini" ];
-then 
-    m1=$(stat -c %Y $line/NTUSER.DAT);
-    m2=$(stat -c %Y $line/ntuser.ini);
-    delta=$(echo $((m1-m2)) | tr -d '-');
+for se3_root_username in /home/profiles/*.V2;
+do
+    [ "$se3_root_username" = '/home/profiles/*.V2' ] && continue;
+    se3_username="${se3_root_username##*/}";
+    se3_username="${se3_username%.V2}";
+    if [ -f "$se3_root_username/NTUSER.DAT" ] && [ -f "$se3_root_username/ntuser.ini" ];
+    then 
+        m1=$(stat -c %Y $se3_root_username/NTUSER.DAT);
+        m2=$(stat -c %Y $se3_root_username/ntuser.ini);
+        delta=$(echo $((m1-m2)) | tr -d '-');
 
-    if [ "$delta" -gt "60" ];
-    then
-      echo $line2>>/home/netlogon/delProfile2.txt;
-      echo "$line2 : Profil corrompu";
+        if [ "$delta" -gt "60" ];
+        then
+            echo $se3_username>>/home/netlogon/delProfile2.txt;
+            echo "$se3_username : Profil corrompu";
+        fi
+        else
+        m1=0;
+        m2=0;
+        if [ -f "$se3_root_username/NTUSER.DAT" ];
+        then
+            echo $se3_username>>/home/netlogon/delProfile2.txt;
+            echo "$se3_username : Profil corrompu (1)";
+        fi
+        if [ -f "$se3_root_username/ntuser.ini" ];
+        then
+            echo $se3_username>>/home/netlogon/delProfile2.txt;
+            echo "$se3_username : Profil corrompu (2)";
+        fi
     fi
-else
-    m1=0;
-    m2=0;
-    if [ -f "$line/NTUSER.DAT" ];
-    then
-        echo $line2>>/home/netlogon/delProfile2.txt;
-        echo "$line2 : Profil corrompu (1)";
-    fi
-    if [ -f "$line/ntuser.ini" ];
-    then
-        echo $line2>>/home/netlogon/delProfile2.txt;
-        echo "$line2 : Profil corrompu (2)";
-    fi
-fi
 done
 cat /home/netlogon/delProfile2.txt>>/home/netlogon/delProfile.txt;
 rm /home/netlogon/delProfile2.txt;
