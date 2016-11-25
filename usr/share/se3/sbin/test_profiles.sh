fc37e8cb957c443439b57275039918152d80b6b7
lolo4992 <laurentjoly@orange.fr>
2016-11-25 15:26:19 +0100

    modification du test profile
    Correction des acls

--- usr/share/se3/sbin/test_profiles.sh
+++ usr/share/se3/sbin/test_profiles.sh
@@ -11,11 +11,26 @@ chown -R root:root /home/netlogon/delProfile2.txt;
 chmod 755 /home/netlogon/delProfile2.txt;
 
 #echo "Debut de test des profils";
-for se3_root_username in /home/profiles/*.V2;
+cd /
+for se3_root_username in home/profiles/*.V2;
 do
     [ "$se3_root_username" = '/home/profiles/*.V2' ] && continue;
     se3_username="${se3_root_username##*/}";
     se3_username="${se3_username%.V2}";
+	
+#echo "Verification des droits";
+if [ -z $(getfacl $se3_root_username | grep "^other::rwx") ];
+then
+    setfacl -b $se3_root_username;
+    setfacl -Rm user::rwx $se3_root_username;
+    setfacl -Rm group::rwx $se3_root_username;
+    setfacl -Rm other::rwx $se3_root_username;
+    echo "$se3_username : ACL corrompu par W7";
+# else
+#   echo "Pour une fois W7 ne pete rien";
+fi
+
+#echo "verification du ntuser.dat";
     m1=0;
     m2=0;
     delta=0;
